edcccc2f8a9283a2a1c1c371e709ce8ab8e94bba
MDIsrar <82327888+MDIsrar@users.noreply.github.com>
2025-06-22 16:39:50 +0530

    push notification integration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
+import { useEffect } from 'react';
+import { Alert, PermissionsAndroid, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import messaging from "@react-native-firebase/messaging"
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+
+  const requestPermission = async ()=>{
+    try {
+      const result = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+      console.log("result**", result)
+      console.log("result**2", PermissionsAndroid.RESULTS.GRANTED)
+      if(result === PermissionsAndroid.RESULTS.GRANTED){
+        // request for device token
+        requestToken()
+      }else{
+        Alert.alert("Permission Denied")
+      }
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  const requestToken = async ()=>{
+    try {
+      await messaging().registerDeviceForRemoteMessages();
+      const token = await messaging().getToken();
+      console.log("token**", token)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  useEffect(()=>{
+    requestPermission()
+  }, [])
+
+
+  /** foreground notification */
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text>Push Notification</Text>
     </View>
   );
 }
@@ -22,6 +55,8 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent:"center",
+    alignItems:'center'
   },
 });
 
